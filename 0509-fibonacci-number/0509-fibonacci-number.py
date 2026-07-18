b6dd05b4ec31dfdc68945c88866fdcfde30d25f8
Subhasis Mahapatra <redmahapatra579@gmail.com>
2026-07-19 01:31:50 +0530

    Time: 270 ms (37.74%), Space: 19.3 MB (18.42%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,9 +2,11 @@
 class Solution:
     
     def fib(self, n: int) -> int:
-        if n <=1:
+        if n <= 1:
             return n 
-        return self.fib(n-1) + self.fib(n-2)
-#problem ..runs in O(n2) .. need to add a sequence to lower the tc . 
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+
+
 
         
