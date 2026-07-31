11b0f7d534eaaf7c5c12df0822d2ebb96c6b3f3b
Subhasis Mahapatra <redmahapatra579@gmail.com>
2026-07-31 09:01:26 +0530

    Time: 179 ms (35.45%), Space: 19.9 MB (77.26%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        ch=[0]*26
-        push=1
-        res=0
-        count=1
-        for i in word :
-            ch[ord(i)-97] +=1
+        ch = [0] * 26
+        
+        for c in word:
+            ch[ord(c) - ord('a')] += 1
+        
         ch.sort(reverse=True)
+
+        res = 0
         for i in range(26):
             if ch[i] == 0:
                 break
-            if count > 8:
-                count=0
-                push+=1
-            res +=(ch[i]*push)
-            count+=1
+            
+            push = (i // 8) + 1
+            res += ch[i] * push
+        
         return res
 
 
