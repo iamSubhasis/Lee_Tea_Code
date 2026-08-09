12e41c2609a9fdeb8a5f2f3ccd6ef3ebdc6269d8
Subhasis Mahapatra <redmahapatra579@gmail.com>
2026-08-09 09:34:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.54%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,26 @@
+class MyStack:
+
+    def __init__(self):
+        self.q1 = []
+        self.q2 = []
+
+    def push(self, x: int) -> None:
+        self.q2.append(x)
+
+        while self.q1:
+            self.q2.append(self.q1.pop(0))
+
+        self.q1, self.q2 = self.q2, self.q1
+
+    def pop(self) -> int:
+        if self.empty():
+            return -1
+        return self.q1.pop(0)
+
+    def top(self) -> int:
+        if self.empty():
+            return -1
+        return self.q1[0]
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
