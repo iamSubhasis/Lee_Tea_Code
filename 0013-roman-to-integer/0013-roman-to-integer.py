4c9a97e6c88b3478ed8b23a70a3ce959b57828cb
Subhasis Mahapatra <redmahapatra579@gmail.com>
2026-08-11 00:13:09 +0530

    Time: 7 ms (40.41%), Space: 19.3 MB (57.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,24 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
-        d={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        f=0
-        su=0
-        while True:
-            if f+1< len(s):
-                if d[s[f+1]]> d[s[f]]:
-                    su+=(d[s[f+1]]-d[s[f]])
-                    f+=2
-                else:
-                    su+=d[s[f]]
-                    f+=1
+        d = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        f = 0
+        su = 0
+
+        while f < len(s):
+            if f + 1 < len(s) and d[s[f+1]] > d[s[f]]:
+                su += d[s[f+1]] - d[s[f]]
+                f += 2
             else:
-                break
-        if f != len(s):
-            su+=d[s[f]]
+                su += d[s[f]]
+                f += 1
 
         return su
-            
-
-        
